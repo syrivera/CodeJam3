e4d2fd18badb4f3fcd670a4af094187bb87b3c53
Jordan Yu <jmfefoe@jmfamily.com>
2025-10-16 13:40:42 -0400

    updated database context and programcs

--- CodeJam3b/Context.cs
+++ CodeJam3b/Context.cs
@@ -1,16 +1,27 @@
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using 
+
+using CodeJam3b.Models.Users;
+using CodeJam3b.Models.Lists;
+namespace CodeJam3b.Models.Movies;
 
 
-public class SchoolDbContext(string dbName = "sis") : DbContext // sis = school information system
+public class LetterBoxDbContext(string dbName = "letterbox") : DbContext 
 {
 
 
     private readonly string _connectionHost = "localhost";
     private readonly string _connectionDbName = dbName;
 
-    public DbSet<Student> Students { get; set; }
-    public DbSet<Assignment> Assignments { get; set; }
-    public DbSet<Grade> Grades { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Watchlist> Watchlist { get; set; }
+    public DbSet<Watched> Watched { get; set; }
+    public DbSet<Fav> Fav { get; set; }
+    public DbSet<Diary> Diary { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
